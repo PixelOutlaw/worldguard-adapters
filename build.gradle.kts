c0873a196fb10b0950326be2bf39123cb0623368
Richard Harrah <topplethenunnery@gmail.com>
2021-02-06 22:52:01 -0500

    upgrade to pixeloutlaw-gradle-plugin 3.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,8 @@
 plugins {
     kotlin("jvm") apply false
-    id("nebula.nebula-bintray")
-    id("nebula.release")
-    id("io.pixeloutlaw.multi")
+    id("io.pixeloutlaw.gradle")
 }
 
 subprojects {
     this@subprojects.version = rootProject.version
-}
-
-bintray {
-    pkgName.value("worldguard-adapters")
-    repo.value("pixeloutlaw-jars")
-    userOrg.value("pixeloutlaw")
-    syncToMavenCentral.value(false)
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "6.8.1"
-    distributionType = Wrapper.DistributionType.ALL
 }
