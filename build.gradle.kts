170520ca0c6a2e3a6db3c6af428bd364ad2a6787
Richard Harrah <topplethenunnery@gmail.com>
2021-02-07 00:57:20 -0500

    add missing description

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("io.pixeloutlaw.gradle")
 }
 
+description = "WorldGuard Adapters for MythicDrops"
+
 subprojects {
+    this@subprojects.description = rootProject.description
     this@subprojects.version = rootProject.version
 }
